a807c3f72d15922867a80e2d87f9d7a299bd0265
stakonaite <55126042+stakonaite@users.noreply.github.com>
2019-10-21 15:50:41 +0300

    race

--- index.php
+++ index.php
@@ -1,30 +1,34 @@
 <?php
 
 $race = [
+    'start' => [
+        'direction-rigth' => false,
+        'angle' => 180,
+    ],
     'turn_1' => [
         'direction-rigth' => true,
         'angle' => 90,
-        ],
-        'turn_2' => [
+    ],
+    'turn_2' => [
         'direction-rigth' => false,
         'angle' => 150,
-        ],
-        'turn_3' => [
+    ],
+    'turn_3' => [
         'direction-rigth' => true,
         'angle' => 120,
-        ],
-        'turn_4' => [
+    ],
+    'turn_4' => [
         'direction-rigth' => false,
         'angle' => 160,
-        ],
-        'turn_5' => [
+    ],
+    'turn_5' => [
         'direction-rigth' => true,
         'angle' => 80,
-        ],
-        'turn_6' => [
+    ],
+    'turn_6' => [
         'direction-rigth' => true,
         'angle' => 170,
-        ],
+    ],
 ];
 
 var_dump($race);
